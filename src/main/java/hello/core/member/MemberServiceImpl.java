28b238f474555be8c49a1207434eb6347ee89a96
kaiphojor <jihoopark7666@gmail.com>
2022-01-28 11:18:14 +0900

    add shortcut comment

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+    // ctrl + space -> ctrl + shift + enter 하면 code completion이 semicolon과 함께 된다
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
